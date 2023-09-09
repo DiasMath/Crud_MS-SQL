982b5eee15accc21824d5b801b0182c1112e65ed
Matheus Dias <92406256+DiasMath@users.noreply.github.com>
2023-09-08 23:57:31 -0300

    adicionando tabela, colunas, criacao de classe cliente e contexto e configuracao do dbset

---
+++ Crud_MS-SQL/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Crud_MS_SQL.Models;
+
+
+[Table("Cliente")]
+public class Cliente
+{
+    [Column("Id")]
+    [Display(Name = "Cógido")]
+    public int Id { get; set; }
+
+    [Column("Nome")]
+    [Display(Name = "Nome")]
+    public string Nome { get; set; }
+
+
+    [Column("Email")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Column("Telefone")]
+    [Display(Name = "Telefone")]
+    public string Telefone { get; set; }
+
+    [Column("Endereco")]
+    [Display(Name = "Endereço")]
+    public string Endereco { get; set; }
+
+    [Column("Agendamento")]
+    [Display(Name = "Agendamento")]
+    public string Agendamento { get; set; }
+
+    [Column("Servico")]
+    [Display(Name = "Serviço")]
+    public string Servico { get; set; }
+}
---
+++ Crud_MS-SQL/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud_MS_SQL.Models;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Cliente> Cliente { get; set; }
+}
